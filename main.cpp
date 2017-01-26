17cc9195d99ee5ed1a4f84ee91725c67a95138be
sdcole96 <sdcole96@yahoo.com>
2017-01-26 12:10:40 -0800

    NEW1
    NEW

--- main.cpp
+++ main.cpp
@@ -26,13 +26,26 @@ GLfloat light_position[] = {-1.0, 0.0, 2.0, 0.0};
 GLfloat light_position2[] = {1.0, 0.0, 0.0, 0.0};
 GLfloat emit[] = {.5, .5, .5, 0.0};
 GLfloat red[] = {1.0,0.0,0.0,0.0};
-int numOfAsteroids = 99;
-float asteroidX[99];	//arrays for to store asteroid coords
-float asteroidY[99];
-float asteroidZ[99];
+int numOfAsteroids = 1;
+float asteroidX[1];	//arrays for to store asteroid coords
+float asteroidY[1];
+float asteroidZ[1];
+
+float asteroidX2[99];
+float asteroidY2[99];
+float asteroidZ2[99];
+
+
+
+bool upPressed = false;
+bool downPressed = false;
+bool leftPressed = false;
+bool rightPressed = false;
+bool gameOver = false;
 
 bool eyeLevel = false;
-
+float width;
+float height;
 
 void initLighting()
 {
@@ -58,11 +71,11 @@ float random(int min, int max) //create a random number in a specified range
 
 void setUpAsteroids()	//to initialise array of random coords for star within coordinate space
 {
-    for (int i = 0; i < numOfAsteroids; ++i)
+    for(int i = 0; i< numOfAsteroids; i++)
     {
-        asteroidX[i] = random(-150,150)/75.0; // -1.5 - 1.5
-        asteroidY[i] = random(-100,100)/50.0; //-1.0 - 1.0
-        asteroidZ[i] = -5;
+        asteroidX[i] = random(-100,100)/500; // -.5 - .5
+        asteroidY[i] = random(-80,80)/500; //-1.0 - 1.0
+        asteroidZ[i] = (float)(-1)*(i)-10;
     }
 }
 
@@ -71,11 +84,13 @@ void setUpAsteroids()	//to initialise array of random coords for star within coo
 void createAsteroids()
 {
     glLoadIdentity();
-    for(int i = 0; i<numOfAsteroids; ++i)
+    for(int i = 0; i<numOfAsteroids; i++)
     {
         glTranslatef(asteroidX[i],asteroidY[i],asteroidZ[i]);
-        glutSolidSphere(10*scale,50,10);
+        glutSolidSphere(.1,10,10);
     }
+    
+    glLoadIdentity();
     glEnd();
     glutSwapBuffers();
 }
@@ -104,6 +119,8 @@ void createShip(GLfloat orbitRadius, GLfloat radius, GLfloat period) //what crea
 
 void display(void)
 {
+    
+    
     glLoadIdentity();
     GLfloat light_diffuse[] = { 1.0, 1.0, 1.0, 1.0 };
     GLfloat pos[] = { 0.0, 0.0, 1.0, 0.0 };
@@ -113,7 +130,7 @@ void display(void)
 
     glClear (GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
     createShip(0,5,0); //mercury
-    createPlanet(10+2,2,5);
+    //createPlanet(10+2,2,5);
     
     createAsteroids();
     
@@ -126,12 +143,28 @@ void createStars(int intensity)
     
 }
 
+void shipHasBeenHit()
+{
+    for(int i = 0; i< numOfAsteroids; i++)
+    {
+        if(asteroidZ[i] > -1)
+        {
+            if((asteroidX[i] == translateX) || (asteroidY[i] == translateY))
+            {
+                gameOver = true;
+            }
+        }
+    }
+}
+
 void reshape (int w, int h)
 {
+    height = h;
+    width = w;
     glViewport (0, 0, (GLsizei) w, (GLsizei) h);
     glMatrixMode (GL_PROJECTION);
     glLoadIdentity();
-    gluPerspective(60,w/h,.1,1000);
+    gluPerspective(90,w/h,.1,1000);
     
     glMatrixMode(GL_MODELVIEW);
     glLoadIdentity();
@@ -140,19 +173,45 @@ void reshape (int w, int h)
 
 void TimerFunction()
 {
-    t += .1;
-    for(int i = 0; i< numOfAsteroids; ++i)
+    if(gameOver == false)
     {
-        if(asteroidZ[i] < 1)
-        {
-            asteroidZ[i] += .05;
-        }
-        else
-        {
-            asteroidZ[i] = -10.0;
-        }
+    t += .1;
+    shipHasBeenHit();
+    if(leftPressed && translateX > -.75)
+    {
+        translateX -= .05;
     }
+    else if(rightPressed && translateX < .75)
+    {
+        translateX += .05;
+    }
+    if(upPressed && translateY < -.25)
+    {
+        translateY += .05;
+    }
+    else if(downPressed && translateY > -1.75)
+    {
+        translateY -= .05;
+    }
+    
+    for(int i = 0; i< numOfAsteroids; i++)
+    {
+            if(asteroidZ[i] < 2)
+            {
+                asteroidZ[i] += .1;
+            }
+            else
+            {
+                asteroidX[i] = random(-50,50)/100.0; // -.5 - .5
+                asteroidY[i] = random(-100,100)/200; //-1.0 - 1.0
+                asteroidZ[i] -= 10;
+            }
+        
+    }
+     
     glutPostRedisplay();
+    glutSwapBuffers();
+    }
 }
 
 void mouse(int x, int y)
@@ -165,39 +224,65 @@ void specialInput(int key, int x, int y)
     switch(key)
     {
         case 'a':   //if it isn't too far left, moves sphere to the left
-            if(translateX > -.5)
-            {
-            translateX -= .25;
-            }
+        {
+            leftPressed = true;
             break;
+        }
         case 'd':   //if it isn't too far right, moves sphere to the left
-            if(translateX < .5)
-            {
-            translateX += .25;
-            }
+        {
+            rightPressed = true;
             break;
+        }
         case 'w':   //if it isn't too far up, moves sphere up
         {
-            if(translateY < -.5)
-            {
-            translateY += .25;
-            }
+            upPressed = true;
             break;
         }
         case 's':   //if it isn't too far down, moves sphere down
         {
-            if(translateY > -1.5)
-            {
-            translateY -= .25;
-            }
+            downPressed = true;
             break;
         }
     }
     glutPostRedisplay();
 }
 
+void specialInputUp(int key, int x, int y)
+{
+    switch(key)
+    {
+        case 'a':   //if it isn't too far left, moves sphere to the left
+        {
+            leftPressed = false;
+            break;
+        }
+        case 'd':   //if it isn't too far right, moves sphere to the left
+        {
+            rightPressed = false;
+            break;
+        }
+        case 'w':   //if it isn't too far up, moves sphere up
+        {
+            upPressed = false;
+            break;
+        }
+        case 's':   //if it isn't too far down, moves sphere down
+        {
+            downPressed = false;
+            break;
+        }
+    }
+    glutPostRedisplay();
+}
+
+
+
+
+
 int main(int argc,char * argv[])
 {
+    srand(time(NULL));
+    setUpAsteroids();
     glutInit(&argc, argv);
     glutInitDisplayMode (GLUT_SINGLE | GLUT_RGB | GLUT_DEPTH);
     glutInitWindowSize (1000, 1000);
@@ -205,15 +290,15 @@ int main(int argc,char * argv[])
     glutCreateWindow (argv[0]);
     glutDisplayFunc(display);
     glutSpecialFunc(specialInput);
+    glutSpecialUpFunc(specialInputUp);
     glutIdleFunc(TimerFunction);
     glutReshapeFunc(reshape);
     glutPassiveMotionFunc(mouse);
     initLighting();
     glEnable(GL_DEPTH_TEST);
     // Initialization
-    glOrtho(-1.0, 1.0, -1.0, 1.0, 1.0, 20.0);
+    glOrtho(0, 1000, 0, 1000, 0, 1000);
     glutAttachMenu(GLUT_LEFT_BUTTON);
-    setUpAsteroids();
     glutMainLoop();
     return 0;
 }
